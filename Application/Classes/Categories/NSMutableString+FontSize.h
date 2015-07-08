2727e829c81ace4075ec2aece11e4ecedc7d644c
TUONG DANG <DKTUONG@DANGs-MacBook-Pro.local>
2015-07-08 12:02:54 +0700

    Commit

---
+++ Application/Classes/Categories/NSMutableString+FontSize.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableString+FontSize.h
+//  CoAssets
+//
+//  Created by TUONG DANG on 7/8/15.
+//  Copyright (c) 2015 Sanyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSAttributedString (FontSize)
+- (NSAttributedString*) attributedStringWithFontSize:(CGFloat) fontSize;
+@end
