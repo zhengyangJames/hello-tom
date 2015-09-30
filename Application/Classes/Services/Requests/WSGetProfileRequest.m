b36b57c05aaf5a84daefe420b9ee47f41f07a1aa
Tony Tuong <TonyTuong@Tonys-MacBook-Pro.local>
2015-09-30 15:33:46 +0700

    Commit

--- Application/Classes/Services/Requests/WSGetProfileRequest.m
+++ Application/Classes/Services/Requests/WSGetProfileRequest.m
@@ -13,13 +13,20 @@
 @implementation WSGetProfileRequest
 
 -(void)setValueWithTokenData:(NSDictionary *)tokenData {
-    NSString *postString = [self paramsToString:tokenData];
-     NSData *parambody = [postString dataUsingEncoding:NSUTF8StringEncoding];
-    [self setHTTPBody:parambody];
-    if ([tokenData objectForKey:kTOKEN_TYPE] && [tokenData objectForKey:kACCESS_TOKEN]) {
-        NSString *value = [NSString stringWithFormat:@"%@ %@",[tokenData  valueForKey:kTOKEN_TYPE],[tokenData valueForKey:kACCESS_TOKEN]];
-        [[NSURLCache sharedURLCache] removeAllCachedResponses];
-        [self setValue:value forHTTPHeaderField:@"Authorization"];
-    }
+    
+    [[NSURLCache sharedURLCache] removeAllCachedResponses];
+    NSString *headerString = [NSString stringWithFormat:@"%@ %@",[tokenData objectForKeyNotNull:kTOKEN_TYPE], [tokenData objectForKeyNotNull:kACCESS_TOKEN]];
+    NSDictionary *headers = @{ @"authorization": headerString };
+    [self setHTTPMethod:METHOD_GET];
+    [self setAllHTTPHeaderFields:headers];
+    
+//    NSString *postString = [self paramsToString:tokenData];
+//     NSData *parambody = [postString dataUsingEncoding:NSUTF8StringEncoding];
+//    [self setHTTPBody:parambody];
+//    if ([tokenData objectForKey:kTOKEN_TYPE] && [tokenData objectForKey:kACCESS_TOKEN]) {
+//        NSString *value = [NSString stringWithFormat:@"%@ %@",[tokenData  valueForKey:kTOKEN_TYPE],[tokenData valueForKey:kACCESS_TOKEN]];
+//        [[NSURLCache sharedURLCache] removeAllCachedResponses];
+//        [self setValue:value forHTTPHeaderField:@"Authorization"];
+//    }
 }
 @end
